6c9796a98d813c548de3143556221626b46d7a5f
Chguzenda <chguzenda@crimson.ua.edu>
2024-09-19 14:24:38 -0500

    Update webots_ros2_homework1_python.py

--- webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
+++ webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
@@ -74,7 +74,7 @@ class RandomWalk(Node):
         (posx, posy, posz) = (position.x, position.y, position.z)
         (qx, qy, qz, qw) = (orientation.x, orientation.y, orientation.z, orientation.w)
         self.get_logger().warn('self position: {},{},{}'.format(posx,posy,posz))
-        self.get_logger().info('furthest position: {},{}'.format(posx,posy));
+        self.get_logger().info('furthest position: {},{}'.format(self.greatestX,self.greatestY));
         # similarly for twist message if you need
         self.pose_saved=position
         
